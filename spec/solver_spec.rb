09db1ea0c8b3d58089e1b7b53d633a237012edef
Terbeche Mustapha <termus96@gmail.com>
2022-09-02 21:17:05 +0100

    Create unit test for reverse  method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -17,4 +17,12 @@ describe Solver do
       expect(@solver.factorial(5)).to eq 120
     end
   end
+
+  describe '#reverse' do
+    it 'it reverses the string' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+      expect(@solver.reverse('moon')).to eq 'noom'
+      expect(@solver.reverse('call911')).to eq '119llac'
+    end
+  end
 end
