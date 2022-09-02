e7d685b38c62b6b00380cd8d3bdc6c0789cce7a5
Terbeche Mustapha <termus96@gmail.com>
2022-09-02 21:16:20 +0100

    Create factorial method to make test pass

--- Solver.rb
+++ Solver.rb
@@ -1,3 +1,15 @@
 class Solver
-
+  def factorial(number)
+    if number.negative?
+      'This is not a positive number'
+    elsif number.zero?
+      1
+    else
+      res = 1
+      (1..number).each do |i|
+        res *= i
+      end
+      res
+    end
+  end
 end
