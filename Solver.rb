f2e19082951f548186328429518322234e4a29a8
Terbeche Mustapha <termus96@gmail.com>
2022-09-02 21:18:56 +0100

    Create fizzbuzz method to make test pass

--- Solver.rb
+++ Solver.rb
@@ -21,4 +21,16 @@ class Solver
     puts res
     res
   end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
 end
